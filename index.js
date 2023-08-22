226db672e3cf699b23c4d59494ebf6ad763019f9
kelshardy <14024@coderacademy.edu.au>
2023-08-22 20:43:54 +1000

    Create npm project

---
+++ index.js
@@ -0,0 +1,36 @@
+// const promptInstance = require('prompt-sync')({sigint:true}); - another way of writing the code below
+
+const promptSync = require('prompt-sync');
+
+const promptInstance = promptSync({
+    sigint: true // allows CTRL+C to exit the app
+});
+
+let userInput = null;
+let userEnteredNumber = false;
+do {
+    userInput = promptInstance("What is the coolest number in the universe?");
+
+    let userInputAsNumber = Number(userInput);
+
+    if (userInputAsNumber === 42){
+        console.log("The universe thanks you for figuring out the answer!");
+        userEnteredNumber = true;
+    } else {
+        console.log("The universe seems unfulfilled...");
+    }
+} while (!userEnteredNumber);
+
+// helloWorld(userInput);
+
+// let {messagesToShare} = require('./messageList');
+
+// function helloWorld(someMessageToSay){
+//     console.log(someMessageToSay);
+// }
+
+// helloWorld("hello there!");
+
+// for (const message of messagesToShare) {
+//     helloWorld(message);
+// }
---
+++ messageList.js
@@ -0,0 +1,15 @@
+let messages = [
+    "Hello Callum!",
+    "Hello Naomi!",
+    "Hello Nick!",
+    "Hello Luke!"
+]
+
+function sayMessages(){
+    console.log(messages);
+}
+// sayMessages();
+
+module.exports = {
+    messagesToShare: messages
+}
